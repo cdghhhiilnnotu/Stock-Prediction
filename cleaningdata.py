485ae6590b1d0099373ce00c292e31250090da22
cdghhhiilnnotu <nguyenthanhduong22072002@gmail.com>
2023-08-30 01:44:19 +0700

    first commit

---
+++ cleaningdata.py
@@ -0,0 +1,93 @@
+import csv
+from datetime import datetime
+import os
+import matplotlib.pyplot as plt
+
+
+def load_data(filename):
+    myListDate = []
+    myListOpen = []
+    myListHigh = []
+    myListLow = []
+    myListClose = []
+    myListTotal = []
+
+    with open(filename) as stocks:
+        dataFile = csv.reader(stocks)
+        next(dataFile)
+        for row in dataFile:
+            if len(myListDate) > 0:
+                if row[1].split(' ')[0] != myListDate[len(myListDate) - 1]:
+                    tempList = [myListDate[len(myListDate) - 1], myListOpen[len(myListOpen) - 1], myListHigh[len(myListHigh) - 1], myListLow[len(myListLow) - 1], myListClose[len(myListClose) - 1]]
+                    myListTotal.append(tempList)
+                    # Get List Date
+                    myListDate.append(row[1].split(' ')[0])
+                    # Get List Open
+                    myListOpen.append(row[2])
+                    myListHigh.append(row[3])
+                    myListLow.append(row[2])
+                    myListClose.append(row[5])
+                else:
+                    # Get List Close
+                    myListClose[len(myListClose) - 1] = row[5]
+                    
+                    # Get List High
+                    if(myListHigh[len(myListHigh) - 1] < row[3]):
+                        myListHigh[len(myListHigh) - 1] = row[3]
+
+                    # Get List Low
+                    if(myListLow[len(myListLow) - 1] > row[4]):
+                        myListLow[len(myListLow) - 1] = row[4]
+                # if len(myListTotal) == 10:
+                #     break
+            else:
+                myListDate.append(row[1].split(' ')[0])
+                myListOpen.append(row[2])
+                myListHigh.append(row[3])
+                myListLow.append(row[2])
+                myListClose.append(row[5])
+    return myListTotal
+
+def write_data(listData, filename):
+    if os.path.exists(filename):
+        os.remove(filename)
+        print(f"Existed {filename}. Deleted File!")
+    with open(filename, 'w', newline='') as myFile:
+        wr = csv.writer(myFile)
+        listHead = ['Date', 'Open', 'High', 'Low', 'Close']
+        wr.writerow(listHead)
+        for word in listData:
+            wr.writerow(word)
+
+
+def Plot_Data(filename, idxFig):
+    listTotal = load_data(filename)
+    listDate = [datetime.strptime(item[0], '%m/%d/%Y') for item in listTotal]
+    listOpen = [float(item[1]) for item in listTotal]
+    listHigh = [float(item[2]) for item in listTotal]
+    listLow = [float(item[3]) for item in listTotal]
+    listClose = [float(item[4]) for item in listTotal]
+
+    plt.subplot(2,2,idxFig)
+    # plt.plot(listDate, listOpen, label="Open", color="green", marker="o", mfc="black", ms=2, linewidth=1)
+    # plt.plot(listDate, listOpen, label="Open", color="green")
+    plt.plot(listDate, listHigh, label="High", color="blue", linewidth=1)
+    plt.plot(listDate, listLow, label="Low", color="red", linewidth=1)
+    # plt.plot(listDate, listClose, label="Open", color="yellow")
+    plt.title(filename)
+    plt.xlabel("Date")
+    plt.ylabel("Value")
+
+if __name__ == "__main__":
+    for file in os.listdir('Data'):
+        write_data(load_data(f'Data/{file}'), f'Clean/Clean-{file}')
+
+    plt.show()
+
+
+
+
+
+
+
+
---
+++ mainPNJ.py
@@ -0,0 +1,76 @@
+import math
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+
+total_data = pd.read_csv('Clean/Clean-PNJ.csv')
+data = total_data.filter(['Close'])
+dataset = data.values
+train_len = math.ceil(len(dataset) * 0.7)
+
+scaler = MinMaxScaler(feature_range=(0,1))
+data_scaled = scaler.fit_transform(dataset)
+
+data_train = data_scaled[0:train_len, :]
+x_train = []
+y_train = []
+
+for i in range(60, len(data_train)):
+    x_train.append(data_train[i-60:i])
+    y_train.append(data_train[i])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+model = Sequential()
+model.add(LSTM(60, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(30, return_sequences=False))
+model.add(Dense(15))
+model.add(Dense(1))
+
+model.compile(
+    loss=keras.losses.MeanSquaredError(),
+    optimizer=keras.optimizers.Adam(lr=0.1)
+)
+model.fit(x_train, y_train, batch_size=1, epochs=3)
+
+data_test = data_scaled[train_len - 60:, :]
+x_test = []
+y_test = dataset[train_len:, :]
+
+for i in range(60, len(data_test)):
+    x_test.append(data_test[i-60:i, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+
+rmse = np.sqrt(np.mean(predictions - y_test)**2 )
+print(rmse)
+
+train = data[:]
+valid = data[train_len:]
+valid['Predictions'] = predictions
+
+plt.title('Model Prediction for PNJ')
+plt.xlabel('Date')
+plt.ylabel('Close Price')
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Training', 'Valid','Predictions'])
+plt.show()
+
+print(predictions[-1])
+
+
+
+
+
+
